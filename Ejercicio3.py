d498769f50f65636e2c49e44578fafccc35fa6b7
Candela-Lopez-Nanio <candelalopeznanio81@gmail.com>
2022-08-13 02:29:15 -0300

    subo ejercicio 3 de Python

---
+++ Ejercicio3.py
@@ -0,0 +1,10 @@
+##3)  Crear un lista en Python denominado “Perro” que contenga los siguientes valores:
+##13,  Puppy,  12/12/2012 , Macho, 123
+##Que se corresponde con los datos de un perro de nuestra base de datos (Id_Perro, nombre, fecha de nacimiento,
+##sexo y dni del dueño). Modificar la fecha de nacimiento por 13/12/2012 y reemplazar “dni del dueño” por 28957346 .
+
+Perro = [13,  "Puppy",  12/12/2012 , "Macho", 123]
+
+Perro[2]= "12/12/2012"
+Perro[3]=28957346
+print(Perro)
